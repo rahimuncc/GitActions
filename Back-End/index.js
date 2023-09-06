894378aded3bbb54b38e8de68bf6507032b64d13
rahimuncc <rahimun@cyclecircle.one>
2023-09-06 09:43:09 +0530

    added yml

--- Back-End/index.js
+++ Back-End/index.js
@@ -11,6 +11,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(path.join(_dirname, 'C:\Users\n1606\Desktop\Jobs\CC\V1\CC_v1\GitHubActions\Front_End/build')))
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
